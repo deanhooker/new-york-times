99cd0f7167a03f1a6f64f21bc58382de11741c8f
Nicholas Moore <nmoore@Nicholass-MacBook-Pro.local>
2017-12-09 13:12:54 -0700

    added vars and btns to JS

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,21 @@
+var searchTerm = "";
+var startYear = 0;
+var endYear = 0;
+var numRecords = 0;
+
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
   'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931"
+});
+
+// start button
+$('#startBtn').on('click', function(){
+
+});
+
+// clear button reloads DOM
+$('clearBtn').on('click', function(){
+  location.reload();
 });
 
 $.ajax({
